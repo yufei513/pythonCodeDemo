ff6eb8ce72c99506e8077d71fefcaf8cee404050
xiaodong <yongxiaodong1@gmail.com>
2025-03-05 00:59:57 +0800

    LONDON

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello. CI/CD!"
+    return "Hello. LONDON"
 # 测试
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5013)
