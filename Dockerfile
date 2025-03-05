e21ed28193352a7ebbedf48d94a2043ec1b564b8
xiaodong <yongxiaodong1@gmail.com>
2025-03-05 13:52:17 +0800

    x

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,14 @@
 FROM python:3.9-slim
 
 # 设置工作目录
-WORKDIR /app
+WORKDIR /app3
 
 # 复制依赖文件并安装
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
 # 复制应用代码
-COPY app.py .
+COPY app.py /app2/
 
 # 运行应用
-CMD ["python", "app.py"]
+CMD ["python", "/app/app.py"]
--- app.py
+++ app.py
@@ -4,7 +4,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello. LONDON"
+    return "If you see me on the web page, prove that you have completed all the tasks"
+#def check_network():
+#    try:
+#        result = subprocess.run(
+#            ["ping", "-c", "2", "baidu.com"], 
+#            stdout=subprocess.PIPE, 
+#            stderr=subprocess.PIPE
+#        )
+#        return result.returncode == 0
+#    except Exception as e:
+#        print(f"网络检测异常: 我好像需要ping 通baidu.com才能启动成功: {e}")
+#        return False
+
 # 测试
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5013)
+    #if not check_network():
+    #    print("我没有网络，如果我无法访问baidu.com,我就会一直拒绝启动")
+    #    sys.exit(1)
+    print("tips: 我在容器内监听的端口是80")
+    app.run(host="0.0.0.0", port=80)
+
